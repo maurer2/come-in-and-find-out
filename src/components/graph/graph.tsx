da0d94c41cb06eee26c13393473eaf51d15d624b
Martin <mr.mmaurer@gmail.com>
2024-07-28 20:22:22 +0100

    Improved graph

--- src/components/graph/graph.tsx
+++ src/components/graph/graph.tsx
@@ -8,15 +8,16 @@ import {
   type XYChartTheme,
 } from "@visx/xychart";
 import { scaleLinear } from "@visx/scale";
+import { Group } from "@visx/group";
 
 type GraphProps = {
   width: number;
   height: number;
 };
 
-const data = [...Array(12).keys()].map((value) => ({ x: value, y: value }));
+const data = [...Array(11).keys()].map((value) => ({ x: value, y: value }));
 const smallestDataEntry = data[0];
-const largestDataEntry = data.at(-1);
+const largestDataEntry = data[data.length - 1];
 
 const theme: XYChartTheme = buildChartTheme({
   backgroundColor: "black",
@@ -29,69 +30,89 @@ const theme: XYChartTheme = buildChartTheme({
 const margin: Margin = {
   top: 0,
   right: 0,
-  bottom: 100,
-  left: 150,
+  bottom: 40,
+  left: 40,
 };
 
-const Graph = ({ width, height }: GraphProps) => (
-  <XYChart
-    margin={margin}
-    width={width + margin.left + margin.right}
-    height={height + margin.top + margin.bottom}
-    xScale={{
-      type: "linear",
-      range: [0, width],
-      domain: [smallestDataEntry.x, largestDataEntry?.x ?? 0],
-    }}
-    yScale={{
-      type: "linear",
-      range: [0, height],
-      domain: [largestDataEntry?.y ?? 0, smallestDataEntry.y],
-    }}
-    theme={theme}
-  >
-    <Axis
-      orientation="left"
-      numTicks={12}
-      top={-100}
-      left={50}
-      stroke="white"
-      tickLabelProps={{
-        fill: "white",
-        fontSize: 12,
+const axisSizes = 40;
+const verticalTopSpacing = 10; // prevent top number getting cut off
+const horizontalRightSpacing = 8; // prevent top number getting cut off
+
+const Graph = ({ width, height }: GraphProps) => {
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+
+  return (
+    <XYChart
+      margin={margin}
+      width={innerWidth}
+      height={innerHeight}
+      xScale={{
+        type: "linear",
+        range: [0, innerWidth - axisSizes - horizontalRightSpacing],
+        domain: [smallestDataEntry.x, largestDataEntry.x],
       }}
-    />
-
-    <Axis
-      orientation="bottom"
-      numTicks={12}
-      stroke="white"
-      top={500}
-      left={50}
-      tickLabelProps={{
-        fill: "white",
-        fontSize: 12,
+      yScale={{
+        type: "linear",
+        range: [0, innerHeight - axisSizes - verticalTopSpacing],
+        domain: [largestDataEntry.y, smallestDataEntry.y],
       }}
-    />
-
-    {/* <Grid columns={true} numTicks={12} /> */}
-
-    <LineSeries
-      dataKey="line"
-      data={data}
-      xAccessor={(d) =>
-        scaleLinear({
-          range: [0, 12],
-        })(d.x)
-      }
-      yAccessor={(d) =>
-        scaleLinear({
-          range: [0, 12],
-        })(d.y)
-      }
-      stroke="red"
-    />
-  </XYChart>
-);
+      theme={theme}
+    >
+      <Group top={axisSizes} left={0}>
+        <Axis
+          orientation="left"
+          numTicks={data.length}
+          top={-axisSizes + verticalTopSpacing}
+          tickLabelProps={{
+            fill: "white",
+            fontSize: 16,
+            dx: -8,
+            textAnchor: "end",
+            verticalAnchor: "end",
+          }}
+          left={axisSizes}
+          stroke="white"
+          strokeWidth={1}
+        />
+        <Axis
+          orientation="bottom"
+          numTicks={data.length}
+          top={innerHeight - axisSizes - axisSizes}
+          left={axisSizes}
+          tickLabelProps={{
+            fill: "white",
+            fontSize: 16,
+            dy: 8,
+            textAnchor: "middle",
+            verticalAnchor: "end",
+          }}
+          stroke="white"
+          strokeWidth={1}
+        />
+        <Grid
+          columns={true}
+          numTicks={1}
+          left={axisSizes}
+          top={-axisSizes + verticalTopSpacing}
+        />
+        <LineSeries
+          dataKey="line"
+          data={data}
+          xAccessor={(d) =>
+            scaleLinear({
+              range: [smallestDataEntry.x, largestDataEntry.x],
+            })(d.x)
+          }
+          yAccessor={(d) =>
+            scaleLinear({
+              range: [smallestDataEntry.y, largestDataEntry.y],
+            })(d.y)
+          }
+        />
+      </Group>
+    </XYChart>
+  );
+};
 
 export default Graph;
