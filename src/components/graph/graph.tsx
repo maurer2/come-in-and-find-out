9afaa0905f090c3c2479b49dedd631cde913567c
Martin <mr.mmaurer@gmail.com>
2024-08-06 23:05:09 +0100

    Added bottom label

--- src/components/graph/graph.tsx
+++ src/components/graph/graph.tsx
@@ -1,6 +1,5 @@
 import {
   Axis,
-  Grid,
   LineSeries,
   XYChart,
   buildChartTheme,
@@ -40,7 +39,8 @@ const margin: Margin = {
   left: 40,
 };
 
-const axisSizes = 40;
+const axisSizeY = 40;
+const axisSizeX = 120;
 const verticalTopSpacing = 10; // prevent top number getting cut off
 const verticalBottomSpacing = 20; // space between custom label and x-axis
 const horizontalRightSpacing = 8; // prevent right number getting cut off
@@ -50,7 +50,7 @@ const Graph = ({ width, height, labelXAxis, labelYAxis }: GraphProps) => {
   const labelYWidth = getStringWidth(labelYAxis) ?? 0;
   const labelXWidth = getStringWidth(labelXAxis) ?? 0;
 
-  const innerWidth = width + -margin.left - margin.right;
+  const innerWidth = width - margin.left - margin.right;
   const innerHeight = height - margin.top - margin.bottom;
 
   return (
@@ -61,20 +61,36 @@ const Graph = ({ width, height, labelXAxis, labelYAxis }: GraphProps) => {
       xScale={{
         type: "linear",
         // prettier-ignore
-        range: [0, innerWidth - axisSizes - horizontalRightSpacing - labelYWidth - horizontalLeftSpacing,
+        range: [0, innerWidth - axisSizeY - horizontalRightSpacing - labelYWidth - horizontalLeftSpacing,
         ],
         domain: [smallestDataEntry.x, largestDataEntry.x],
       }}
       yScale={{
         type: "linear",
         // prettier-ignore
-        range: [0, innerHeight - axisSizes - verticalTopSpacing],
+        range: [0, innerHeight - axisSizeX - axisSizeY - verticalTopSpacing],
         domain: [largestDataEntry.y, smallestDataEntry.y],
       }}
       theme={theme}
+      accessibilityLabel="Graph"
     >
-      <Group top={axisSizes} left={labelYWidth + horizontalLeftSpacing}>
+      <Group top={axisSizeY} left={labelYWidth + horizontalLeftSpacing}>
         <Group>
+          <Axis
+            orientation="left"
+            numTicks={data.length}
+            top={verticalTopSpacing}
+            tickLabelProps={{
+              fill: "white",
+              fontSize: 16,
+              dx: -8,
+              textAnchor: "end",
+              verticalAnchor: "end",
+            }}
+            left={axisSizeY}
+            stroke="white"
+            strokeWidth={6}
+          />
           <Text
             width={labelYWidth}
             fontSize={40}
@@ -87,40 +103,13 @@ const Graph = ({ width, height, labelXAxis, labelYAxis }: GraphProps) => {
           >
             {labelYAxis}
           </Text>
-          <Axis
-            orientation="left"
-            numTicks={data.length}
-            top={-axisSizes + verticalTopSpacing}
-            tickLabelProps={{
-              fill: "white",
-              fontSize: 16,
-              dx: -8,
-              textAnchor: "end",
-              verticalAnchor: "end",
-            }}
-            left={axisSizes}
-            stroke="white"
-            strokeWidth={6}
-          />
         </Group>
-        <Group>
-          <Text
-            fontSize={40}
-            x={(innerWidth - axisSizes) / 2 - axisSizes - labelXWidth / 2}
-            width={labelXWidth}
-            y={innerHeight - axisSizes - axisSizes + verticalBottomSpacing - 50}
-            fill="white"
-            fontWeight={300}
-            textAnchor="start"
-            fontVariant="all-small-caps"
-          >
-            {labelXAxis}
-          </Text>
+        <Group top={axisSizeX}>
           <Axis
             orientation="bottom"
             numTicks={data.length}
-            top={innerHeight - axisSizes - axisSizes}
-            left={axisSizes}
+            left={axisSizeY}
+            top={innerHeight - axisSizeX - axisSizeX - axisSizeY}
             tickLabelProps={{
               fill: "white",
               fontSize: 16,
@@ -131,17 +120,23 @@ const Graph = ({ width, height, labelXAxis, labelYAxis }: GraphProps) => {
             stroke="white"
             strokeWidth={6}
           />
+          <Text
+            fontSize={40}
+            x={innerWidth - (innerWidth * 2) / 3}
+            y={innerHeight - axisSizeX - axisSizeY - verticalTopSpacing}
+            width={labelXWidth}
+            fill="white"
+            fontWeight={300}
+            textAnchor="start"
+            fontVariant="all-small-caps"
+          >
+            {labelXAxis}
+          </Text>
         </Group>
-        <Grid
-          columns={true}
-          numTicks={11}
-          left={axisSizes}
-          top={-axisSizes + verticalTopSpacing}
-        />
       </Group>
       <Group
-        top={verticalTopSpacing}
-        left={axisSizes + labelYWidth + horizontalLeftSpacing}
+        top={verticalTopSpacing + axisSizeY}
+        left={axisSizeY + labelYWidth + horizontalLeftSpacing}
       >
         <LineSeries
           dataKey="line"
