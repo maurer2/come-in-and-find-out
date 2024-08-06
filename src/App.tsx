3039980471c79f77bc20e8cba65ad0197386a0fd
Martin <mr.mmaurer@gmail.com>
2024-08-06 22:33:03 +0100

    Added responsive scaling

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
+import { useScreenSize } from '@visx/responsive';
+
 import { useQueryParam } from "./hooks/useQueryParams/useQueryParam";
 import Graph from "./components/graph/graph";
 
+
 function App() {
-  // todo responsive stuff
+  const { width: widthScreen } = useScreenSize({ debounceTime: 50 });
 
   const labelXAxis = useQueryParam("label") || "Mess around";
   const labelYAxis = "Find out";
+  const size = Math.min(widthScreen, 1024);
 
   return (
     <main className="flex min-h-svh align-items">
       <figure className="m-auto">
         <Graph
-          width={800}
-          height={600}
+          width={size}
+          height={size}
           labelXAxis={labelXAxis}
           labelYAxis={labelYAxis}
         />
